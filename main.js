51dccbbd7d30f0b9007d0e94659211aa6010b63a
Amirni19 <amir19nigmatullin.19@gmail.com>
2024-09-26 19:49:18 +0300

    practis-15

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict'
+let interval;
+const colorBlock = document.getElementById('colorBlock');
+
+function getRandomColor() {
+    const letters = '01234ABC567DEF89';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.getElementById('startButton').onclick = () => {
+    interval = setInterval(() => {
+        colorBlock.style.backgroundColor = getRandomColor();
+    }, 500);
+};
+
+document.getElementById('stopButton').onclick = () => {
+    clearInterval(interval);
+};
